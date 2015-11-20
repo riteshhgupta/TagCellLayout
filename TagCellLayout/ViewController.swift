419498d84f28114c8b1599c23e77ae8a1808dfcf
Ritesh Gupta <rg.riteshh@gmail.com>
2015-11-20 17:22:42 +0530

    Initial Commit

---
+++ TagCellLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TagCellLayout
+//
+//  Created by Ritesh-Gupta on 20/11/15.
+//  Copyright © 2015 Ritesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
